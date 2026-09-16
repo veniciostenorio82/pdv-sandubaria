401b458a6772c799fe300290cf333020f77f72b8
Venus741 <veniciostenorio1@gmail.com>
2026-09-16 04:34:38 -0300

    teste2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,7 +8,6 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error', 'warn'],
-    
   });
 
 if (process.env.NODE_ENV !== 'production') {
